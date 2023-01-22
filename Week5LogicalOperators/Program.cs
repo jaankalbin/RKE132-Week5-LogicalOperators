6057857b621890cacf4d208f592bda14efcd1298
Jaan Kalbin <jaan.kalbin@tktk.ee>
2023-01-22 22:50:55 +0200

    add Week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
